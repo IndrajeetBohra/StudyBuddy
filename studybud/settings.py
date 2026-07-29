055ce5a40cb8e9107f049e5aede318ffe3d7827f
IndrajeetBohra <bohraindrajeet@gmail.com>
2026-07-29 22:36:01 +0530

    Django REST framework

--- studybud/settings.py
+++ studybud/settings.py
@@ -39,16 +39,24 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
+
+    'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
 ]
 
 ROOT_URLCONF = 'studybud.urls'
@@ -125,3 +133,5 @@ STATICFILES_DIRS = [
 ]
 
 # STATIC_ROOT = 
+
+CORS_ALLOW_ALL_ORIGINS = True
