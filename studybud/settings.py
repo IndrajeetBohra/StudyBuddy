db56495d99813efcf85126ce771feba69344a9e5
IndrajeetBohra <bohraindrajeet@gmail.com>
2026-07-30 21:26:47 +0530

    Integrating Custom User Model. Install Pilow. Tutorial complete.

--- studybud/settings.py
+++ studybud/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "corsheaders",
 ]
 
+AUTH_USER_MODEL = 'base.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
@@ -127,10 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+#in prod aws s3 bucket
 
 # STATIC_ROOT = 
 
